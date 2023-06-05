7e8ba85945dd0239cf5ad55b31e5145c6f007da3
hsmtkk <hsmtkk@gmail.com>
2023-06-05 16:05:49 +0900

    privileged mode is needed to build docker

--- terraform/code_build.tf
+++ terraform/code_build.tf
@@ -28,9 +28,10 @@ resource "aws_codebuild_project" "code_build" {
   name         = var.project
   service_role = aws_iam_role.code_build.arn
   environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type         = "LINUX_CONTAINER"
-    image        = "aws/codebuild/standard:7.0"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    type            = "LINUX_CONTAINER"
+    image           = "aws/codebuild/standard:7.0"
+    privileged_mode = true
   }
   artifacts {
     type = "NO_ARTIFACTS"
